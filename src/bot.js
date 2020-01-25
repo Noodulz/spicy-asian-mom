3688a1776160c9a97df2f8f2a4a2c7810c5e6045
Jocelyn Dzuong <jocelyndzuong@ufl.edu>
2020-01-25 18:35:00 -0500

    Added comments for clarity

--- src/bot.js
+++ src/bot.js
@@ -1,32 +1,47 @@
+// Imports Discord API
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
+// Terms to identify, though not sure if this is case-sensitive. Will have to try out later 
 const term = "study";
 const anotherTerm = "STUDY";
 const yetAnotherTerm = "Study";
 const anotherAnotherTerm = "studying";
 const buildUponAnotherStudy = "Studying";
+
+// Array of random messages mom bot sends. Will add more, or delete the nice ones
 var listOfAnswers = ['Yucky daughter', 'GO STUDY RIGHT NOW U UNGRATEFUL DAUGHTER', 'YOU DISAPPOINTMENT', 'WHY U BORN SO DUMB', 'YOU DISAPPOINT YOUR FAMILY', 'GO DO HOMEWORK! NOW!!!', 'YOUR COUSIN IS DOCTOR ALREADY!!', 'Stop think about girls and think about FUTURE!', 'DISGRACEFUL. GO STUDY', 'YOU DISHONOR ME BY NOT STUDYING!', 'Still no study? GO GET THE RICE PADDLE', 'NO RICE FO YU', 'NO WORK?? NO BOBA MONEY', 'GO TO THE CORNER AND KNEEL', 'NO FRY RICE FOR YOU', 'WHY U NO STUDY AND GET MARRY YET??', 'YOU GET CANCER IF YOU NO STUDY', 'A-SIAN, NOT B-SIAN', 'You forget to study? I forget to feed you.', 'You get below C, I PUT YOU BELOW SEA', 'WHY U GET F IN GENDER HUH????', 'Be sure to drink lots of water before you study, hun',
     'Here, have some fruits and go study', 'Thank goodness there\'s ONE smart child in this family *glares at other siblings*', 'Good luck!', 'You are favorite child', 'I\'m not ashamed to be your mother', 'You so smart', 'U 1 in billion, now study and get me 1 billion money', 'You have brought your family honor!', 'I believe you can become doctor', 'You did good job... today', 'Well well! You aren\'t always a total disappointment', 'I believe you can become doctor'
 ];
+
+// Turns on bot to detect received messages in server. receivedMessage as an alias for "message"
 client.on("message", (receivedMessage) => {
-    // Prevent bot from responding to its own receivedMessages
+    // Sends a confirmation message to terminal confirming that bot does indeed work
     console.log("It works!!");
+    // Prevents bot from responding to itself, like anti-recursion kind of
     if (receivedMessage.author == client.user) {
         return;
         if (message.author.bot) {
             return;
         }
     }
+    // Might've been to bypass the promises error. Checks to see if the message is blank or not in the first few chars
     if (!receivedMessage.content[0] == " " && !receivedMessage.content[1] == "") {
+        // Checks the user's message for any of the terms mentioned above related to studying
         if ((receivedMessage.content.includes(term) || receivedMessage.content.includes(anotherTerm) || receivedMessage.content.includes(yetAnotherTerm))) {
+            // Gets a random number to call from the array of messages to send
             var randomNum = Math.floor((Math.random((listOfAnswers.length + 1)) * listOfAnswers.length));
             var getAnAnswer = listOfAnswers[randomNum];
+            // Converts array object to a string that's to be sent
             var asianResponse = String.toString(getAnAnswer);
+            // Finally parses and sends the message back
             receivedMessage.channel.send(getAnAnswer);
         }
     }
 
 })
 
-bot_token = ''; // Token hidden for privacy.
+// Token hidden for privacy. Get your own token pls
+bot_token = ''; 
+// Logs into Discord API with token and activates this bot
 client.login(bot_token);
